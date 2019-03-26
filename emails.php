a288685ebb73e05df63039b5d928c6674fc816a1
notkeywitt <keywitt@gmail.com>
2019-03-26 19:43:12 -0400

    emails

--- emails.php
+++ emails.php
@@ -1,32 +1,5 @@
-<!doctype html>
-<html>
-	
-	<head>
-	<meta http-equiv="refresh" content="5; URL='http://adarush.com'">
-		<style type="text/css">
-		 <!––html {overflow:hidden;/}-->
-		</style>
-		<title>Ada Rush</title>
-		<link href="adarush.css" rel="stylesheet" type="text/css"><script>var __adobewebfontsappname__="dreamweaver"</script>
-		<script src="http://use.edgefonts.net/source-sans-pro:n2:default.js" type="text/javascript"></script>
-	</head>
-																
-	<body>
-		
-		<h1>  Thanks, your mail has been sent. </h1>
-			
-  <?php
-    // The message
-
-    $email_address = $_GET["emails"];
-    $message = $_GET["message"];
-
-    $subject = 'Someone just emailed you from your own site';
-
-    mail('keywitt@gmail.com', $email_address);
-  ?>
-		
-
-
-	
-</body></html>
+<?php
+if($_POST["emails"]) {
+    mail("your@email.address", "Form to email message", $_POST["emails"], "From: an@email.address");
+}
+?>
